d1e8c7ffa5474add9c316e2ea93a5a034041a82f
Nikolay Belibov <nikolay.belibov@dstarlab.com>
2021-06-30 11:29:12 +0300

    add server route, add start script for ENV

--- scripts/start.js
+++ scripts/start.js
@@ -1,14 +1,37 @@
+const path = require('path');
 const Hapi = require('@hapi/hapi');
 
 const port = process.env.PORT || 3000;
+const { NODE_ENV } = process.env;
+const FILES = /\.(js|js.map|woff|woff2|svg|bmp|jpg|jpeg|gif|png|ico)(\?v=\d+\.\d+\.\d+)?$/;
+const PATH = {
+  '/': 'index.html',
+};
+const pathFromEnvMode = NODE_ENV === 'production'
+  ? 'build'
+  : 'dist';
+
+console.log('Server: => NODE_ENV: ', NODE_ENV);
+console.log('pathFromEnvMode: ', pathFromEnvMode);
 
 const init = async () => {
   const server = Hapi.server({
     port,
   });
-
   // eslint-disable-next-line global-require
   await server.register(require('@hapi/inert'));
+
+  server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: (request, h) => {
+      if (FILES.test(request.path)) {
+        return h.file(path.join(process.cwd(), pathFromEnvMode, request.path));
+      }
+
+      return h.file(path.join(process.cwd(), pathFromEnvMode, PATH[request.path]));
+    },
+  });
 
   await server.start();
   console.log('Server running on %s', server.info.uri);
