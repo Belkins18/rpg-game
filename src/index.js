ade1a1bde0e0cdaa5ac23bf7cffec2f6ba8e4794
Nikolay Belibov <nikolay.belibov@dstarlab.com>
2021-07-02 17:33:15 +0300

    canvas, strokeRect, fiilRect, clearRect

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './index.scss';
 
+const CANVAS_ID = 'game';
+
 console.log('####: INIT :####');
+const canvas = document.getElementById(CANVAS_ID);
+const ctx = canvas.getContext('2d');
+
+// // first
+// ctx.strokeStyle = 'red';
+// ctx.lineWidth = 5;
+// ctx.strokeRect(20, 20, 200, 100);
+//
+// // second
+// ctx.fillStyle = '#e4e4e4';
+// ctx.fillRect(200, 200, 300, 150);
+//
+// ctx.fillStyle = 'rgb(145, 121, 100)';
+// ctx.fillRect(100, 400, 200, 150);
+//
+// ctx.clearRect(0, 0, 300, 300)
+
+let xy = 0;
+setInterval(() => {
+    ctx.clearRect(0, 0, 600, 600)
+    xy += 10;
+    ctx.fillStyle = '#e4e4e4';
+    ctx.fillRect(xy, xy, 100, 50);
+}, 120)
