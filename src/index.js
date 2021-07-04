6f1afed98de97034f821a8637aa74f69978bc3d6
Belkins18 <nikolay.belibov@gmail.com>
2021-07-05 00:08:19 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,112 +2,123 @@ import './index.scss';
 import SenseiWalk from './assets/heroes/Male-3-Walk.png';
 
 const CANVAS_ID = 'game';
+const KEYBOARD_ACTIONS = {
+  UP: {
+    KEY: 'ArrowUp',
+    IS_PRESSED: false,
+    HERO_SPRITES_POSITION: 144,
+  },
+  DOWN: {
+    KEY: 'ArrowDown',
+    IS_PRESSED: false,
+    HERO_SPRITES_POSITION: 0,
+  },
+  LEFT: {
+    KEY: 'ArrowLeft',
+    IS_PRESSED: false,
+    HERO_SPRITES_POSITION: 48,
+  },
+  RIGHT: {
+    KEY: 'ArrowRight',
+    IS_PRESSED: false,
+    HERO_SPRITES_POSITION: 96,
+  },
+};
 
 console.log('####: INIT :####');
 const canvas = document.getElementById(CANVAS_ID);
 const ctx = canvas.getContext('2d');
 
-const spriteW = 48;
-const spriteH = 48;
-const shots = 3;
+const GAME_STATIC_PROPS = {
+  spriteW: 48,
+  spriteH: 48,
+  shots: 3,
+};
+
 let cycle = 0;
-let heroDirection = 0;
+let direction = 0;
 let pY = 270;
 let pX = 270;
 
-// eslint-disable-next-line no-unused-vars
-let upPresed = false;
-let bottomPresed = false;
-let leftPresed = false;
-let rightPresed = false;
-
 function keyDownHandler(e) {
-  if (e.key === 'Up' || e.key === 'ArrowUp') {
-    upPresed = true;
-  }
-  if (e.key === 'Down' || e.key === 'ArrowDown') {
-    bottomPresed = true;
-  }
-  if (e.key === 'Left' || e.key === 'ArrowLeft') {
-    leftPresed = true;
-  }
-  if (e.key === 'Right' || e.key === 'ArrowRight') {
-    rightPresed = true;
+  const { UP, DOWN, LEFT, RIGHT } = KEYBOARD_ACTIONS;
+
+  if (!e.repeat) {
+    switch (e.key) {
+      case UP.KEY:
+        UP.IS_PRESSED = true;
+        break;
+      case DOWN.KEY:
+        DOWN.IS_PRESSED = true;
+        break;
+      case LEFT.KEY:
+        LEFT.IS_PRESSED = true;
+        break;
+      case RIGHT.KEY:
+        RIGHT.IS_PRESSED = true;
+        break;
+      default:
+        console.log(`Key "${e.key}" pressed  [event: keydown]`);
+        break;
+    }
+  } else {
+    console.log(`Key "${e.key}" repeating  [event: keydown]`);
   }
 }
 
 function keyUpHandler(e) {
-  if (e.key === 'Up' || e.key === 'ArrowUp') {
-    upPresed = false;
-  }
-  if (e.key === 'Down' || e.key === 'ArrowDown') {
-    bottomPresed = false;
-  }
-  if (e.key === 'Left' || e.key === 'ArrowLeft') {
-    leftPresed = false;
-  }
-  if (e.key === 'Right' || e.key === 'ArrowRight') {
-    rightPresed = false;
+  const { UP, DOWN, LEFT, RIGHT } = KEYBOARD_ACTIONS;
+  switch (e.key) {
+    case UP.KEY:
+      UP.IS_PRESSED = false;
+      break;
+    case DOWN.KEY:
+      DOWN.IS_PRESSED = false;
+      break;
+    case LEFT.KEY:
+      LEFT.IS_PRESSED = false;
+      break;
+    case RIGHT.KEY:
+      RIGHT.IS_PRESSED = false;
+      break;
+    default:
+      break;
   }
 }
 
 document.addEventListener('keydown', keyDownHandler);
-document.addEventListener('keyr', keyDownHandler);
 document.addEventListener('keyup', keyUpHandler);
-
-// eslint-disable-next-line no-unused-vars
-const hero = {
-  direction: {
-    down: 0,
-    left: 48,
-    right: 96,
-    up: 144,
-  },
-};
 
 const img = document.createElement('img');
 img.src = SenseiWalk;
 
 img.addEventListener('load', () => {
+  const { UP, DOWN, LEFT, RIGHT } = KEYBOARD_ACTIONS;
+  const { spriteW, spriteH, shots } = GAME_STATIC_PROPS;
+
   setInterval(() => {
-    if (upPresed && pY > 0) {
-      heroDirection = hero.direction.up;
+    if (UP.IS_PRESSED && pY > 0) {
+      direction = UP.HERO_SPRITES_POSITION;
       pY -= 10;
       cycle = (cycle + 1) % shots;
-      console.log({
-        pX,
-        pY,
-      });
     }
-    if (bottomPresed && pY < 550) {
-      heroDirection = hero.direction.down;
+    if (DOWN.IS_PRESSED && pY < 550) {
+      direction = DOWN.HERO_SPRITES_POSITION;
       pY += 10;
       cycle = (cycle + 1) % shots;
-      console.log({
-        pX,
-        pY,
-      });
     }
-    if (leftPresed && pX > 0) {
-      heroDirection = hero.direction.left;
+    if (LEFT.IS_PRESSED && pX > 0) {
+      direction = LEFT.HERO_SPRITES_POSITION;
       pX -= 10;
       cycle = (cycle + 1) % shots;
-      console.log({
-        pX,
-        pY,
-      });
     }
-    if (rightPresed && pX < 550) {
-      heroDirection = hero.direction.right;
+    if (RIGHT.IS_PRESSED && pX < 550) {
+      direction = RIGHT.HERO_SPRITES_POSITION;
       pX += 10;
       cycle = (cycle + 1) % shots;
-      console.log({
-        pX,
-        pY,
-      });
     }
 
     ctx.clearRect(0, 0, 600, 600);
-    ctx.drawImage(img, cycle * spriteW, heroDirection, spriteW, spriteH, pX, pY, 48, 48);
+    ctx.drawImage(img, cycle * spriteW, direction, spriteW, spriteH, pX, pY, 48, 48);
   }, 120);
 });
