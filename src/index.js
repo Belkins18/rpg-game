1b1b6c5ffbdeb9f7201774771ade8de017c78fe8
Belkins18 <nikolay.belibov@gmail.com>
2021-07-04 17:44:17 +0300

    draw Image

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import SenseiWalk from './assets/heroes/Male-3-Walk.png';
 
 const CANVAS_ID = 'game';
 
@@ -6,24 +7,28 @@ console.log('####: INIT :####');
 const canvas = document.getElementById(CANVAS_ID);
 const ctx = canvas.getContext('2d');
 
-// // first
-// ctx.strokeStyle = 'red';
-// ctx.lineWidth = 5;
-// ctx.strokeRect(20, 20, 200, 100);
-//
-// // second
-// ctx.fillStyle = '#e4e4e4';
-// ctx.fillRect(200, 200, 300, 150);
-//
-// ctx.fillStyle = 'rgb(145, 121, 100)';
-// ctx.fillRect(100, 400, 200, 150);
-//
-// ctx.clearRect(0, 0, 300, 300)
+const spriteW = 48;
+const spriteH = 48;
+const shots = 3;
+let cycle = 0;
 
-let xy = 0;
-setInterval(() => {
-    ctx.clearRect(0, 0, 600, 600)
-    xy += 10;
-    ctx.fillStyle = '#e4e4e4';
-    ctx.fillRect(xy, xy, 100, 50);
-}, 120)
+// eslint-disable-next-line no-unused-vars
+const hero = {
+  direction: {
+    down: 0,
+    left: 48,
+    right: 96,
+    up: 144,
+  },
+};
+
+const img = document.createElement('img');
+img.src = SenseiWalk;
+
+img.addEventListener('load', () => {
+  setInterval(() => {
+    cycle = (cycle + 1) % shots;
+    ctx.clearRect(0, 0, 600, 600);
+    ctx.drawImage(img, cycle * spriteW, 0, spriteW, spriteH, 0, 0, 100, 100);
+  }, 120);
+});
